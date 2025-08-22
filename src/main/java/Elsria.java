5df34fa569df7a489376d22da27da762f5e82a9e
Fang Han <foo.fang.han@gmail.com>
2025-08-22 12:57:29 +0800

    Added echo and bye feature

--- src/main/java/Elsria.java
+++ src/main/java/Elsria.java
@@ -1,19 +1,57 @@
-public class ChatBot {
-    private static final String lineSeparator = "____________________________________________________________\n";
-    private final String name;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
 
-    public ChatBot(String name) {
-        this.name = name;
+public class Elsria {
+    private static final String lineSeparator = "____________________________________________________________";
+    private static final String name = "Elsria";
+
+    public static ArrayList<String> parseString(String input) {
+        ArrayList<String> solution = new ArrayList<>();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            switch (c) {
+                case '\n':
+                    solution.add(sb.toString().strip());
+                    sb.setLength(0);
+                    break;
+                default:
+                    sb.append(c);
+            }
+        }
+        if (sb.length() > 0) {
+            solution.add(sb.toString().strip());
+        }
+        return solution;
     }
 
-    public String introduction() {
-        return String.format("""
-            Hello! I'm %s.
-            What can I do for you today?
-        """, this.name);
+    public static void say(String message) {
+        ArrayList<String> parsedMessage = parseString(message);
+        System.out.println("\t" + lineSeparator);
+        for (String s : parsedMessage) {
+            System.out.println("\t" + s);
+        }
+        System.out.println("\t" + lineSeparator);
     }
 
-    public void say(String message) {
-        System.out.println(message + "\n" + lineSeparator);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        say(String.format("Heya! It's me, %s!\nWhat do you wanna do today?", name));
+
+        boolean running = true;
+        String prompt = "";
+        while (running) {
+            prompt = sc.nextLine();
+            switch (prompt.toLowerCase(Locale.ROOT)) {
+                case "bye":
+                    say("Okey dokey, see you soon!");
+                    running = false;
+                    break;
+                default:
+                    say(prompt);
+            }
+        }
+
     }
 }
