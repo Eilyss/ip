09738ec21a5e243a1cde8db21e7289c2bc8dafb0
Fang Han <foo.fang.han@gmail.com>
2025-08-25 09:39:12 +0800

    Add Event, a type of Task

---
+++ src/main/java/Event.java
@@ -0,0 +1,20 @@
+public class Event extends Task {
+    private String start;
+    private String end;
+
+    public Event(String description, String start, String end) {
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public char taskType() {
+        return 'E';
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, from: %s to: %s", super.toString(), start, end);
+    }
+}
